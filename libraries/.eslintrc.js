7b98656bbf9a8c39c17d2dd5187a185ddc9cbfe7
Thomas Michael Rosik <rosikt0414@my.uwstout.edu>
2017-02-13 11:12:29 -0600

    Thomas Rosik - Programming and Image files (Initial Post)

---
+++ libraries/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "env": {
+    "browser": true,
+  },
+  "parserOptions": {
+    "ecmaVersion": 6
+  },
+  "rules": {
+    "indent": ["error", 2],
+    "linebreak-style": ["error","unix"],
+    "quotes": ["error","single"],
+    "semi": ["error","always"],
+    "no-console": ["warn", { "allow": ["info", "error"] }]
+  }
+};
---
+++ libraries/animalObjects.js
@@ -0,0 +1,26 @@
+//instantiates the Raccoon object
+Raccoon = {
+  sprite : '',
+  jumping : false,
+  jumpHeight : 350,
+  spacePush : false,
+  lowestHeight : 700
+};
+
+//instantiates the Goose object
+Goose = {
+  sprite : '',
+  jumping : false,
+  jumpHeight : 350,
+  spacePush : false,
+  lowestHeight : 700
+};
+
+//instantiates the Possum object
+Possum = {
+  sprite : '',
+  jumping : false,
+  jumpHeight : 350,
+  spacePush : false,
+  lowestHeight : 700
+};
