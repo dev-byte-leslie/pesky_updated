38a04358feec373187f8576717942f64bf769b94
Thomas Michael Rosik <rosikt0414@my.uwstout.edu>
2017-02-15 10:10:44 -0600

    AI follow Player/add constant gravity to player

--- libraries/EnemyAI.js
+++ libraries/EnemyAI.js
@@ -1,5 +1,5 @@
 function aiMovemnt() {
-  if (aCObject.x < 0) {
+  /*if (aCObject.x < 0) {
     aCObject.x = 1000;
   }
 
@@ -9,5 +9,35 @@ function aiMovemnt() {
 
   if (aCObject.vx == 0) {
     aCObject.vx = 5;
+  }*/
+
+  if (aCObject.y > 700) {
+    aCObject.y = 700;
+  }
+
+//if player is to the right of enemy
+  if (aCObject.x < player.sprite.x) {
+    aCObject.vx = 3.5;
+  }
+
+//if player is to the left of enemy
+  if (aCObject.x > player.sprite.x) {
+    aCObject.vx = -3.5;
+  }
+
+//if player is below enemy
+  if (aCObject.y < player.sprite.y) {
+    aCObject.vy = 3.5;
+  }
+
+//if player is above enemy
+  if (aCObject.y > player.sprite.y) {
+    aCObject.vy = -3.5;
+  }
+
+  //if player is next to enemy
+  if (b.hitTestRectangle(aCObject, player.sprite)) {
+    aCObject.vy = 0;
+    aCObject.vx = 0;
   }
 }
