590e1374206c8fe5226ee1b82ef4d658afcf7074
murles29 <murles29@gmail.com>
2017-02-15 10:41:39 -0600

    Initial Commit
    Initial commit for character switch: pseudocode algorithm and framework.

---
+++ characterSwitch.js
@@ -0,0 +1,45 @@
+/********************************************************************
+*
+*File: characterSwitch.js
+*Project: Pesky
+*Author: Leslie Murphy
+*Description: JavaScript code that implements switching chracters.
+*Date: 2-15-17
+*Comments: Inital pseudocode framework. Inital git upload.
+*
+*********************************************************************/
+
+// TODO: Check that player is in the correct spot to move into hedge
+// if(player is in correct area)
+// {
+//    if(TODO: Player presses the correct button)
+//    {
+//        TODO: Player moves into hedge area aniamtion
+//    }
+// }
+//
+// displayMenu()
+//
+// TODO: New player is returned to game by leaving the hedge
+//
+// ========================= Function Definitions ============================== //
+// function displayMenu()
+// {
+//    TODO: Menu pops up for character selection
+//    handleInput()
+//    TODO: Menu dissappears
+//    TODO: Object is changes from original one to the newly selected one
+// }
+//
+// function handleInput()
+// {
+//    TODO: Handle user input on menu - switch statements
+//    TODO: User selects character by pressing button
+//    changeCharacter(object1, object2)
+// }
+//
+// function changeCharacter(object1, object2)
+// {
+//    TODO: Changes the currently active object from object1 to
+//          object2, possible return new object?
+// }
