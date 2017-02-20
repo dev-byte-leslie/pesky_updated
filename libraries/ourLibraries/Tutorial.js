c66d3ac4f598498e4a4941c50bdd71e113b87f2d
Hakaan256 <hyingd7172@my.uwstout.edu>
2017-02-20 11:33:39 -0600

    Fixed typo

--- libraries/ourLibraries/Tutorial.js
+++ libraries/ourLibraries/Tutorial.js
@@ -1,10 +1,10 @@
 var tutorialGroup = new PIXI.Container();
 buttonBack = createButton(renderer.width * 0.15, renderer.height * .85, mainMenu, optionsGroup, 'back');
 tutorialGroup.addChild(buttonBack); // this button is reused for credits and tutorial
+var tutorial = new PIXI.Text('use arrow keys to move');
+tutorialGroup.addChild(tutorial);
 g.stage.addChild(tutorialGroup);
 
 function mainMenu() {
   g.state = menuState;
 }
-
-var tutorial = new PIXI.Text('use arrow keys to move');
