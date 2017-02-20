5aaabc230ede3050d07eb4364c0202b82fe42faa
murles29 <murles29@gmail.com>
2017-02-20 10:42:22 -0600

    adding switch character implementation with menu

--- libraries/ourLibraries/Main.js
+++ libraries/ourLibraries/Main.js
@@ -33,6 +33,15 @@ function tutorialState() {
   tutorialGroup.visible = true;
 }
 
+function switchCharacterState()
+{
+  g.scaleToWndow();
+  hideAll();
+
+}
+
+
+
 // Hide all stage elements
 function hideAll() {
   for (var i = 0; i < g.stage.children.length; i++) {
