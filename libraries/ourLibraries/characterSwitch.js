39eb6859eb299aa1df75f95adf03c51cce42fa5d
murles29 <murles29@gmail.com>
2017-02-22 10:47:33 -0600

    Still working on getting switch character menu buttons to work

--- libraries/ourLibraries/characterSwitch.js
+++ libraries/ourLibraries/characterSwitch.js
@@ -5,55 +5,52 @@
 *Author: Leslie Murphy
 *Description: JavaScript code that implements switching chracters.
 *Date: 2-15-17
-*Comments: Inital pseudocode framework. Inital git upload.
+*Comments:
 *
 *********************************************************************/
 
 var switchCharacterGroup = new PIXI.Contaner(); //Container for objects on switch character menu
-buttonRaccoon = createButton(x, y, handleInput, switchCharacterGroup, spriteName); //TODO: sprintName, and x and y position(center of button)
-buttonSkunk = createButton(x, y, handleInput, switchCharacterGroup, spriteName) //TODO: sprintName, and x and y position(center of button)
-buttonGoose = createButton(x, y, handleInput, switchCharacterGroup, spriteName) //TODO: sprintName, and x and y position(center of button)
+buttonRaccoon = createButton(x, y, raccoonInput, switchCharacterGroup, spriteName); //TODO: sprintName, and x and y position(center of button)
+buttonSkunk = createButton(x, y, skunkInput, switchCharacterGroup, spriteName); //TODO: sprintName, and x and y position(center of button)
+buttonGoose = createButton(x, y, gooseInput, switchCharacterGroup, spriteName); //TODO: sprintName, and x and y position(center of button)
 
- function switchCharacter()
+function switchCharacter()
  {
      // TODO: animate the player up
      // TODO: set current active to false, it disappears.
 
-      g.state = switchCharacterState; //a sort of pause state for game
+  g.state = switchCharacterState; //a sort of pause state for game
     // TODO: Display menu
 
-    handleInput()
+  handleInput();
 
 //    TODO: Menu dissappears
 //    TODO: Object is changes from original one to the newly selected one
- }
+}
 
- function handleInput()
- {
+
    //https://github.com/kittykatattack/tink#buttons
 
-   //TODO: implement button presses in if statements
-    if(buttonRaccoon.action == "pressed")
-    {
-      Raccoon.active = true;
-      player = Raccoon;
-      //TODO: draw new player
-    }
+ //TODO: implement button presses in if statements
+function raccoonInput()
+  {
+  Raccoon.active = true;
+  player = Raccoon;
+    //TODO: draw new player
+}
 
 
-    if(/*user pressed skunk button*/)
-    {
-      Skunk.active = true;
-      player = Skunk;
-      //TODO: draw new player
+function skunkInput()
+  {
+  Skunk.active = true;
+  player = Skunk;
+    //TODO: draw new player
 
-    }
+}
 
-    if(/*user presses goose button*/ )
-    {
-      Goosoe.active = true;
-      player = Goose;
-      //TODO: draw new player
-    }
-
-  }
+function gooseInput()
+  {
+  Goosoe.active = true;
+  player = Goose;
+    //TODO: draw new player
+}
