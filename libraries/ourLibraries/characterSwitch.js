c63de98de9d994389ddf702bda1e44f9ea9dc5cf
murles29 <murles29@gmail.com>
2017-02-22 11:24:01 -0600

    most of character switch implemented

--- libraries/ourLibraries/characterSwitch.js
+++ libraries/ourLibraries/characterSwitch.js
@@ -1,61 +1,63 @@
 /********************************************************************
-*
-*File: characterSwitch.js
-*Project: Pesky
-*Author: Leslie Murphy
-*Description: JavaScript code that implements switching chracters.
-*Date: 2-15-17
-*Comments:
-*
-*********************************************************************/
+ *
+ *File: characterSwitch.js
+ *Project: Pesky
+ *Author: Leslie Murphy
+ *Description: JavaScript code that implements switching chracters.
+ *Date: 2-15-17
+ *Comments:
+ *
+ *********************************************************************/
 
 var switchCharacterGroup = new PIXI.Contaner(); //Container for objects on switch character menu
 buttonRaccoon = createButton(x, y, raccoonInput, switchCharacterGroup, spriteName); //TODO: sprintName, and x and y position(center of button)
 buttonSkunk = createButton(x, y, skunkInput, switchCharacterGroup, spriteName); //TODO: sprintName, and x and y position(center of button)
 buttonGoose = createButton(x, y, gooseInput, switchCharacterGroup, spriteName); //TODO: sprintName, and x and y position(center of button)
 
-function switchCharacter()
-{
-     // -- Determines Which Char Is Active, Animates Up/Sets Inactive-- //
-  if(Raccoon.active)
-    {
-         // TODO: animate the player up
+function switchCharacter() {
+    // -- Determines Which Char Is Active, Animates Up/Sets Inactive-- //
+  if (Raccoon.active)
+  {
+    //TODO: animate the player up
     Raccoon.active = false;
   }
-  else if(Skunk.active)
-    {
-         // TODO: animate the player up
+  else if (Skunk.active)
+  {
+    //TODO: animate the player up
     Skunk.active = false;
   }
-  else if(Gosoe.active)
-    {
-         // TODO: animate the player up
+  else if (Gosoe.active)
+  {
+    //TODO: animate the player up
     Goose.active = false;
   }
 
-  g.state = switchCharacterState; //a sort of pause state for game
+  g.state = switchCharacterState; // -- Displays Menu -- //
 }
 
-//TODO: implement button presses in if statements
+// -- Handes Raccoon Button Press -- //
 function raccoonInput()
 {
   Raccoon.active = true;
   player = Raccoon;
-    //TODO: draw new player
+  g.state = gameState;
+  //TODO: draw new player
 }
 
-
+// -- Handles Skunk Button Press -- //
 function skunkInput()
 {
   Skunk.active = true;
   player = Skunk;
-  // TODO: Switch game state back to game
-    //TODO: draw new player
+  g.state = gameState;
+  //TODO: draw new player
 }
 
+// -- Handles Goose Button Press -- //
 function gooseInput()
 {
   Goosoe.active = true;
   player = Goose;
-    //TODO: draw new player
+  g.state = gameState;
+  //TODO: draw new player
 }
