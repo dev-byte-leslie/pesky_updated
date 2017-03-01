7e8b2dd46d6a5aca9a2e9db6fc363bbc560d59d5
Alex <allthespamineedforever@gmail.com>
2017-03-01 11:41:17 -0600

    Updated tut page
    i changed text elements

--- libraries/ourLibraries/Tutorial.js
+++ libraries/ourLibraries/Tutorial.js
@@ -1,8 +1,8 @@
 var tutorialGroup = new PIXI.Container();
-buttonBack = createButton(WIDTH * 0.15, HEIGHT * .85, mainMenu, optionsGroup, 'back');
-tutorialGroup.addChild(buttonBack); // this button is reused for credits and tutorial
-var tutorial = new PIXI.Text('use arrow keys to move');
+buttonBack = createButton($(document).width() / 2, $(document).height() / 2, mainMenu, tutorialGroup, 'back');
+var tutorial = new PIXI.Text("use arrow keys to move\n\n\navoid animal control\n\n\nwreak havoc\n\n\n", {font: "50px Arial", fill: "red"});
 tutorialGroup.addChild(tutorial);
+tutorialGroup.addChild(buttonBack);
 g.stage.addChild(tutorialGroup);
 
 function mainMenu() {
