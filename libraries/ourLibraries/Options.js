fc6cbaf4fb91c43101f6fe0d102b9e78da00b328
Alex <allthespamineedforever@gmail.com>
2017-03-08 11:53:02 -0600

    added mute button to options page
    it doesn't do anything yet

--- libraries/ourLibraries/Options.js
+++ libraries/ourLibraries/Options.js
@@ -1,8 +1,15 @@
 var optionsGroup = new PIXI.Container();
 let buttonBack = createButton(WIDTH * 0.15, HEIGHT * .85, mainMenu, optionsGroup, 'back');
+var buttonMute = createButton(WIDTH / 2, HEIGHT * 0.5 - 90, muteAudio, optionsGroup, 'mute');
+
 optionsGroup.addChild(buttonBack); // this button is reused for credits and tutorial
+optionsGroup.addChild(buttonMute);
 g.stage.addChild(optionsGroup);
 
 function mainMenu() {
   g.state = menuState;
+}
+
+function muteAudio() {
+// add later
 }
