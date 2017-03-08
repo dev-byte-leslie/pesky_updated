e237b154c2da695569aa2f94f9cad5579610ccb9
Alex <allthespamineedforever@gmail.com>
2017-03-08 11:21:40 -0600

    More updates to tut page
    added graphics (placeholders)
    updated locations

--- libraries/ourLibraries/Tutorial.js
+++ libraries/ourLibraries/Tutorial.js
@@ -1,13 +1,21 @@
 var tutorialGroup = new PIXI.Container();
 buttonBack = createButton(WIDTH * 0.15, HEIGHT * .85, mainMenu, optionsGroup, 'back');
-var tutorial = new PIXI.Text('use arrow keys to move\n\n\navoid animal control\n\n\nwreak havoc\n\n\n', {font: '50px Arial', fill: 'red'});
+var tutorial = new PIXI.Text('use arrow keys to move\n\n\n\navoid animal control\n\n\nwreak havoc\n\n\n', {font: '50px Arial', fill: 'red'});
+
 var keys = PIXI.Texture.fromImage('../../images/keys.png');
 var keysSprite = new PIXI.Sprite(keys);
-keysSprite.position.x = WIDTH/2;
-keysSprite.position.y = HEIGHT * .01;
+keysSprite.position.x = WIDTH * 0.45;
+keysSprite.position.y = HEIGHT * 0.01;
+
+var animalcontrol = PIXI.Texture.fromImage('../../images/animal_control.png');
+var animalcontrolSprite = new PIXI.Sprite(animalcontrol);
+animalcontrolSprite.position.x = WIDTH * 0.45;
+animalcontrolSprite.position.y = HEIGHT * 0.4;
+
 tutorialGroup.addChild(tutorial);
 tutorialGroup.addChild(buttonBack);
 tutorialGroup.addChild(keysSprite);
+tutorialGroup.addChild(animalcontrolSprite);
 g.stage.addChild(tutorialGroup);
 
 function mainMenu() {
