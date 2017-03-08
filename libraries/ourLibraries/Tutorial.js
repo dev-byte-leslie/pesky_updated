26720bcf5ca7d7dcd89086f859e83fd6b8e500ef
Alex <allthespamineedforever@gmail.com>
2017-03-08 10:31:18 -0600

    tut page update

--- libraries/ourLibraries/Tutorial.js
+++ libraries/ourLibraries/Tutorial.js
@@ -1,8 +1,13 @@
 var tutorialGroup = new PIXI.Container();
-buttonBack = createButton($(document).width() / 2, $(document).height() / 2, mainMenu, tutorialGroup, 'back');
+buttonBack = createButton(WIDTH * 0.15, HEIGHT * .85, mainMenu, optionsGroup, 'back');
 var tutorial = new PIXI.Text('use arrow keys to move\n\n\navoid animal control\n\n\nwreak havoc\n\n\n', {font: '50px Arial', fill: 'red'});
+var keys = PIXI.Texture.fromImage('../../images/keys.png');
+var keysSprite = new PIXI.Sprite(keysSprite);
+keysSprite.position.x = WIDTH/2;
+keysSprite.position.y = HEIGHT/2;
 tutorialGroup.addChild(tutorial);
 tutorialGroup.addChild(buttonBack);
+tutorialGroup.addChild(keysSprite);
 g.stage.addChild(tutorialGroup);
 
 function mainMenu() {
