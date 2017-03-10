9f4b9a29ccad835f06ddfcd1d2b8ea32cbecfb59
Alex <allthespamineedforever@gmail.com>
2017-03-10 11:15:49 -0600

    added music
    play with 'b'
    pause with 'c'
    restart with 'd'

--- libraries/ourLibraries/Options.js
+++ libraries/ourLibraries/Options.js
@@ -1,3 +1,50 @@
+//Load the music
+sounds.load([
+  "music/pinklife.wav"
+]);
+
+sounds.whenLoaded = setup;
+
+function setup() {
+  console.log("sounds loaded");
+
+//Create the sounds
+  var music = sounds["music/pinklife.wav"];
+
+  //Make the music loop
+  music.loop = true;
+
+  //Set the music volume
+  music.volume = 0.7;
+
+  //Capture the keyboard events
+  var b = keyboard(66),
+      c = keyboard(67),
+      d = keyboard(68);
+
+  //Control the sounds based on which keys are pressed
+
+  //Play the loaded music sound
+  b.press = function() {
+    if (!music.playing) {
+      music.play();
+    }
+    console.log("music playing");
+  };
+
+  //Pause the music
+  c.press = function() {
+    music.pause();
+    console.log("music paused");
+  };
+
+  //Restart the music
+  d.press = function() {
+    music.restart();
+    console.log("music restarted");
+  };
+}
+
 var optionsGroup = new PIXI.Container();
 let buttonBack = createButton(WIDTH * 0.15, HEIGHT * .85, mainMenu, optionsGroup, 'back');
 var buttonMute = createButton(WIDTH / 2, HEIGHT * 0.5 - 90, muteAudio, optionsGroup, 'mute');
