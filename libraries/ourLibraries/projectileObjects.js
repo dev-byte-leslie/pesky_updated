c46b4b5cd8f00d04505b28c252569bcdbe45387d
murles29 <murles29@gmail.com>
2017-03-20 11:18:55 -0500

    added pesudocode for projectile objects

---
+++ libraries/ourLibraries/projectileObjects.js
@@ -0,0 +1,38 @@
+/********************************************************************
+*
+*File: projectiles.js
+*Project: Pesky
+*Author: Leslie Murphy
+*Description: .js file to hold all info on attack projectiles
+*Date: 3-20-17
+*Comments:
+********************************************************************/
+
+//TODO: add reference in HTML file
+
+ var rabies = {
+   //name:"rabies",
+   //speed: The speed at which the projectile travels
+   //reloadTime: The number of animation cycles after firing before it can be done again
+   //damage: The amount of damage to the target when the projectile explodes ?? MAYBE
+   //range: The maximum range that a projectile will fly
+   // spriteImages:
+ };
+
+ var poop = {
+   //name:"poop",
+   //speed: The speed at which the projectile travels
+   //reloadTime: The number of animation cycles after firing before it can be done again
+   //damage: The amount of damage to the target when the projectile explodes ?? MAYBE
+   //range: The maximum range that a projectile will fly
+   // // spriteImages:
+ };
+
+ var spray = {
+   //name:"spray",
+   //speed: The speed at which the projectile travels
+   //reloadTime: The number of animation cycles after firing before it can be done again
+   //damage: The amount of damage to the target when the projectile explodes ?? MAYBE
+   //range: The maximum range that a projectile will fly
+   // // spriteImages:
+ };
