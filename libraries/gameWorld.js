5057b414e61bd30a775e2ec65178fcb7c0cee675
Thomas Michael Rosik <rosikt0414@my.uwstout.edu>
2017-03-22 11:07:02 -0500

    Added Generate world function

---
+++ libraries/gameWorld.js
@@ -0,0 +1,12 @@
+function createGameWorld() {
+  //TODO:create two arrays: One is positive x direction, other is negative directions
+
+  var negativeX = [10];
+
+  //positiveX has to be one bigger to accomodate 0
+  var positiveX = [11];
+  // TODO:create a separate function that calculates player coordinates into the
+  //    correct location in either array
+  // TODO: Take converted player coordinates and draw to the screen the correct sprites
+  // TODO: Make sure the sprites/objects are deleted when arent onscreen
+}
