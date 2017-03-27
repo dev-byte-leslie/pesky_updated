ff65c75a7cf9ceafbf1d6fb3b95979340e253e78
Alex <allthespamineedforever@gmail.com>
2017-03-27 10:54:02 -0500

    added sounds to options

--- ourLibraries/Options.js
+++ ourLibraries/Options.js
@@ -1,6 +1,7 @@
 //Load the music
 sounds.load([
-  '../sound/music/pinklife.mp3'
+  "../sound/music/GameMusic.wav",
+  "../sound/music/MenuMusic.wav"
 ]);
 
 sounds.whenLoaded = loadSounds;
@@ -8,48 +9,63 @@ sounds.whenLoaded = loadSounds;
 function loadSounds() {
 
 //Create the sounds
-  var music = sounds['../sound/music/pinklife.mp3'];
+var gameMusic = sounds["../sound/music/GameMusic.wav"],
+    menuMusic = sounds["../sound/music/MenuMusic.wav"],
+    jump = sounds["../sound/music/jump.wav"];
 
   //Make the music loop
-  music.loop = true;
+  gameMusic.loop = true;
+  menuMusic.loop = true;
 
   //Set the music volume
-  music.volume = 0.7;
+  gameMusic.volume = 0.7;
+  menuMusic.volume = 0.7;
 
   //Capture the keyboard events
   var b = keyboard(66),
     c = keyboard(67),
-    d = keyboard(68);
+    d = keyboard(68),
+    space = keyboard(32);
 
   //Control the sounds based on which keys are pressed
 
   //Play the loaded music sound
   b.press = function() {
-    if (!music.playing) {
-      music.play();
+    if (!gameMusic.playing) {
+      gameMusic.play();
     }
     console.log('music playing');
   };
 
   //Pause the music
   c.press = function() {
-    music.pause();
+    gameMusic.pause();
     console.log('music paused');
   };
 
   //Restart the music
   d.press = function() {
-    music.restart();
+    gameMusic.restart();
     console.log('music restarted');
   };
+
+  // jump sound
+  space.press = function() {
+    if (!player.jumping) {
+        jump.play();
+    }
+    console.log("jump");
+  }
 }
 var optionsGroup;
 function initOptions() {
   optionsGroup = new PIXI.Container();
   let buttonBack = createButton(WIDTH * 0.15, HEIGHT * .85, mainMenu, optionsGroup, 'back');
   var buttonMute = createButton(WIDTH / 2, HEIGHT * 0.5 - 90, muteAudio, optionsGroup, 'mute');
+  tutorial = new PIXI.Text('just press c to pause music, ignore the button', {font: '50px Arial', fill: 'red'});
 
   optionsGroup.addChild(buttonBack); // this button is reused for credits and tutorial
+  optionsGroup.addChild(tutorial);
   optionsGroup.addChild(buttonMute);
   g.stage.addChild(optionsGroup);
 }
